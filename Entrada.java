a93b2d64c1c522fda7253b4368bdfe75a7511fd9
mushifz <20243rd062@utez.edu.mx>
2025-05-09 09:31:39 -0600

    Se agrego la opcion de apellidos

--- Entrada.java
+++ Entrada.java
@@ -2,6 +2,8 @@ import javax.swing.JOptionPane;
 public class Entrada {
     public static void main(String[] args){
         String nombreUsuario = JOptionPane.showInputDialog("Como se llama tu nombre ");
-        System.out.println("Tu nombre se llama: " + nombreUsuario);
+        String apellidoPaterno = JOptionPane.showInputDialog("Como se llama tu primer apellido ");
+        String apellidoMaterno = JOptionPane.showInputDialog("Como se llama tu segundo apellido ");
+        System.out.println("Tu nombre se llama: " + nombreUsuario + " " + apellidoPaterno + " " + apellidoMaterno);
     }    
 }
